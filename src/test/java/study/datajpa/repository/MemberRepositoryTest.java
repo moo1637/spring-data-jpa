3b82ba329ee5fe56c8821d902bced10c5887776c
kys <moo1637@gmail.com>
2024-06-23 21:37:57 +0900

    Spring Data JPA commit - spring data jpa custom repository example

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -278,4 +278,9 @@ class MemberRepositoryTest {
         List<Member> findMember = memberRepository.findLockByUsername("member1");
     }
 
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
+
 }
