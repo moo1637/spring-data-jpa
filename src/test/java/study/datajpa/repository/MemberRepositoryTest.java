75c5811d3b7f08f2c5b5df1dab970efba3d94ce6
kys <moo1637@gmail.com>
2024-06-23 17:18:46 +0900

    Spring Data JPA commit - jpql select return Type example

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -147,5 +148,17 @@ class MemberRepositoryTest {
         for (Member s : usernameList) {
             System.out.println("s = " + s);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+//        List<Member> list = memberRepository.findListByUsername("AAA");
+//        Member member = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> findMember = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("findMember = " + findMember);
     }
 }
